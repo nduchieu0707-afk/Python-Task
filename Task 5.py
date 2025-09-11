885d724723fc1ba082ffe2d9bbc9278135c9bc73
Duc Hieu <nduchieu0707@gmail.com>
2025-09-12 01:02:31 +0700

    all task

---
+++ Task 5.py
@@ -0,0 +1,11 @@
+print("Calculate the area of a wall.")
+Feed = input("Enter the width in meters: ")
+Width = int(round(float(Feed)))
+
+Feed = input("Enter the height in meters: ")
+Height = int(round(float(Feed)))
+
+print("Result:")
+print(f"Width is {Width} m and height is {Height} m.")
+Area = Width * Height
+print(f"The wall will be {Area} square meters.")
