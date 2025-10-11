109c5ab7c0833cafd2545bf7462dc99ca275c00f
Duc Hieu Nguyen <nduchieu0707@gmail.com>
2025-10-11 18:35:05 +0700

    Update Task 5.py

--- Task 5, Week 5/Task 5.py
+++ Task 5, Week 5/Task 5.py
@@ -1,14 +1,24 @@
-print("Program starting.")
-print("Options:")
-print("1 - Insert word")
-print("2 - Show current word")
-print("3 - Show current word in reverse")
-print("0 - Exit")
-
-def choice():
-    Yourchoice = int(input("Your choice: "))
-    Word = input("Insert word: ")
-    if Yourchoice == 1:
-        print(f"{Word}")
-    return None
-choice()
+def main():
+    print("Program starting.")
+    current_word = "" 
+    
+    while True:
+        print("\nOptions:")
+        print("1 - Insert word")
+        print("2 - Show current word")
+        print("3 - Show current word in reverse")
+        print("0 - Exit")
+        choice = input("Your choice: ")
+        if choice == "1":
+            current_word = input("Insert word: ")
+        elif choice == "2":
+            print(f"Current word - \"{current_word}\"")
+        elif choice == "3":
+            print(f"Word reversed - \"{current_word[::-1]}\"")
+        elif choice == "0":
+            print("Exiting program.\n")
+            break
+        else:
+            print("Unknown option!")
+    print("Program ending.")
+main()
