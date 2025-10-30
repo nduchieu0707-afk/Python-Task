03b138991499e483997546188a1ecf03664aa704
Duc Hieu Nguyen <nduchieu0707@gmail.com>
2025-10-30 19:52:43 +0700

    Update Task 1.py

--- Task 4, Week 4/Task 1.py
+++ Task 4, Week 4/Task 1.py
@@ -5,4 +5,14 @@ if value1 > value2:
     print("Error")
 else:
     for i in range(value1, value2+1):
+
         print(i)
+
+# or:
+# Insert1 = int(input("Insert: "))
+# Insert2 = int(input("Insert: "))
+# sequences = int(Insert2)
+
+# while Insert1 <= Insert2:
+#     print(Insert1, end=" ")
+#     Insert1 += 1
