6a7bdd2145e1b31e44494d8fcb94ab8c1732a21c
Duc Hieu Nguyen <nduchieu0707@gmail.com>
2025-10-30 19:54:32 +0700

    Refactor input handling and loop logic in Task 5

--- Task 4, Week 4/Task 5.py
+++ Task 4, Week 4/Task 5.py
@@ -20,4 +20,34 @@ while running:
 
         Secound_loop = [str(i) for i in range(value1, value2)]
         print(" ".join(Secound_loop))
+
     break
+
+# or:
+# Insert1 = int(input("Insert starting point: "))
+# Insert2 = int(input("Insert starting point: "))
+# Insert3 = int(input("Insert starting point: "))
+
+# if Insert1 > Insert3:
+#     print("Inspection value must be within the range of start and stop. ")
+# elif Insert1 == Insert2:
+#     print("Starting point value must be less than the stopping point value.")
+#     print("Inspection value must be within the range of start and stop.")
+# elif Insert1 == Insert3:
+#     print("First loop - inspection with break:\n")
+#     print("Second loop - inspection with continue: ")
+#     for i in range(Insert1, Insert2):
+#         if i == Insert3:
+#             continue
+#         print(i, end=" ")
+#     print()
+# else:
+#     for i in range (Insert1, Insert3):
+#         print(i, end=" ")
+#     print()
+    
+#     for i in range(Insert1, Insert2):
+#         if i == Insert3:
+#             continue
+#         print(i, end=" ")
+#     print()
